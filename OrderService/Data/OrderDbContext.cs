0583962e1d3186f50522819676a333803ce86622
Diogo Santos <diogodeese@gmail.com>
2025-07-25 22:11:02 +0100

    chore: create local sql databases

--- OrderService/Data/OrderDbContext.cs
+++ OrderService/Data/OrderDbContext.cs
@@ -8,5 +8,17 @@ namespace OrderService.Data
         public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
 
         public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Items)
+                .WithOne()
+                .HasForeignKey("OrderId")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
